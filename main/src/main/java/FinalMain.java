90a482e42c0712216d2976d8b9cac272ba6c2df7
Zhaiyinzhen <yinzhen.zhai@mail.utoronto.ca>
2024-11-22 20:01:08 -0500

    update package

--- main/src/main/java/FinalMain.java
+++ main/src/main/java/FinalMain.java
@@ -1,3 +1,4 @@
+import interface_adapterss.viewmodel;
 import views.ForgetPasswordView;
 import views.LoginView;
 import views.MainView;
@@ -8,7 +9,10 @@ import java.awt.*;
 
 public class FinalMain {
     public static void main(String[] args) {
+
+        viewmodel viewmodel = new viewmodel();
         MainView mainView = new MainView(new LoginView(), new SignUpView(), new ForgetPasswordView());
+        viewmodel.addPropertyChangeListener(mainView);
         mainView.setBackground(Color.blue);
         mainView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainView.setSize(800, 600);
--- main/src/main/java/views/SignUpView.java
+++ main/src/main/java/views/SignUpView.java
@@ -3,4 +3,47 @@ package views;
 import javax.swing.*;
 
 public class SignUpView extends JPanel {
+
+    private JButton signUpButton = new JButton("Sign Up");
+
+    private JTextField nameField = new JTextField();
+
+    private JTextField emailField = new JTextField();
+
+    private JPasswordField passwordField = new JPasswordField();
+
+    private JTextField usernameField = new JTextField();
+
+    private JComboBox<String> verifyQuestionBox;
+
+    private JPasswordField answerField = new JPasswordField();
+
+    public SignUpView() {
+        setLayout(new BoxLayout(SignUpView.this, BoxLayout.Y_AXIS));
+        JLabel titleLabel = new JLabel("Sign Up");
+        JLabel nameLabel = new JLabel("Username:");
+        JLabel passwordLabel = new JLabel("Password:");
+        JLabel emailLabel = new JLabel("email address:");
+        JLabel verifyQuestionLabel = new JLabel("verify question:");
+        JLabel answerLabel = new JLabel("answer:");
+        String[] questions = {
+                "What is your first pet's name",
+                "What's your primary school teacher's name",
+                "What's your nickname",
+                "Where did you born"
+        };
+        verifyQuestionBox = new JComboBox<>(questions);
+
+        add(titleLabel);
+        add(nameLabel);
+        add(nameField);
+        add(passwordLabel);
+        add(passwordField);
+        add(emailLabel);
+        add(emailField);
+        add(verifyQuestionLabel);
+        add(verifyQuestionBox);
+        add(answerLabel);
+        add(answerField);
+    }
 }
